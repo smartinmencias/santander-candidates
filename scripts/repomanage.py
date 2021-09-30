c7600a0629be5e9a572dddd74e3d56ba4648bbdf
smartinmencias <s.martin.mencias@gmail.com>
2021-09-30 23:37:07 +0200

    new version python script

--- scripts/repomanage.py
+++ scripts/repomanage.py
@@ -1,27 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
- 
+
 import argparse
+import json
 import requests
+import fileinput
+import subprocess
 from requests.structures import CaseInsensitiveDict
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-l", "--list", help="Listar los repositorios", action="store_true")
-parser.add_argument("-c", "--create", help="Crear repositorio", action="store_true")
+parser.add_argument("-c", "--create", help="Crear repositorio")
 args = parser.parse_args()
 
-# Aquí procesamos lo que se tiene que hacer con cada argumento
 if args.list:
+
         response = requests.get('http://192.168.99.106:32000/service/rest/v1/repositories', auth=('admin', 'admin123'))
-        print (response.text)
+        open('repos.json', 'wb').write(response.content)
+        with open('repos.json') as file:
+                db = json.load(file)
+                items = []
+                for item in db:
+                        items.append(item["name"])
+                print(items)
+
 
 if args.create:
+        print ("El nombre del repo a crear es:", args.create)
 
-        url = "http://192.168.99.106:32000/service/rest/v1/repositories/docker/hosted"
+        with open('template.json', 'r') as file :
+                filedata = file.read()
 
+        filedata = filedata.replace('NAME_REPO', args.create)
+
+        with open('template.json', 'w') as file:
+                file.write(filedata)
+
+        url = "http://192.168.99.106:32000/service/rest/v1/repositories/apt/hosted"
         headers = CaseInsensitiveDict()
         headers ["Content-Type"] = "application/json"
-
         data = open('template.json')
         resp = requests.post(url, headers=headers, data=data, auth=('admin','admin123'))
         print(resp.status_code)
